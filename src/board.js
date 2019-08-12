a202e47e59f1b9af5e81c9537e076b6d30a6e333
MareK <markust71@hotmail.com>
2019-08-12 21:59:09 +0200

    Pionki stoją

--- src/board.js
+++ src/board.js
@@ -5,9 +5,13 @@ for (let i = 0; i < 8; i++) {
   board[i] = new Array(8);
 }
 //mamy pustą tablice tu trzeba zaimportować figury wedle przykładu dla pionka
-let pawn = new Pawn(6, 0, 'white');
-board[pawn.x][pawn.y] = pawn;
-pawn = new Pawn(6, 1, 'white');
-board[pawn.x][pawn.y] = pawn;
+let pawn;
+for (let i = 0; i < 8; i++) {
+  pawn = new Pawn(6, i, 'white');
+  board[pawn.x][pawn.y] = pawn;
+  pawn = new Pawn(1, i, 'black');
+  board[pawn.x][pawn.y] = pawn;
+}
+// MareK: pionki ustawione
 
 export default board;
