d2600eb410452ff2bf6bfb2eb8b867e38a689451
Marcin <marcinzuchowski@wp.pl>
2019-08-18 23:00:43 +0200

    System turowy + rezygnacja z ruchu

--- src/touched.js
+++ src/touched.js
@@ -1,35 +1,65 @@
 import board from './board';
 
+
+let possibleMoves =[];
+let currentColor = 'white';
+// chosen piece coordinates
+let x = '';
+let y = '';
+
+const resetCoordinates = () => {
+  x = '';
+  y = '';
+};
+
+const unmarkPossMoves = (arr) => {
+  arr.forEach( id => {
+    document.getElementById(id).className = document
+      .getElementById(id)
+      .className.replace(`possibleMove`, '');
+
+    // TODO: rozwiązać tematykę event listenerów sprytniej, przenosząc każdy do osobnego pliku
+    let old_element = document.getElementById(id);
+    let new_element = old_element.cloneNode(true);
+    old_element.parentNode.replaceChild(new_element, old_element);
+  });
+}
+
+
 const touched = e => {
-  const x = e.currentTarget.id[0];
-  const y = e.currentTarget.id[2];
-  if (!board[x][y]) {
-    return;
-  }
-  const possibleMoves = board[x][y].findLegalMoves();
-  for (let el of possibleMoves) {
-    document.getElementById(el).className += ` possibleMove`;
-    document.getElementById(el).addEventListener('click', e => {
-      board[x][y].move(e.currentTarget.id);
-      for (let x = 0; x < board.length; x++) {
-        for (let y = 0; y < board[x].length; y++) {
-          document.getElementById(`${x},${y}`).className = document
-            .getElementById(`${x},${y}`)
-            .className.replace(`possibleMove`, '');
-
-          //TODO: rozwiązać tematykę event listenerów sprytniej, przenosząc każdy do osobnego pliku
-          let old_element = document.getElementById(`${x},${y}`);
-          let new_element = old_element.cloneNode(true);
-          old_element.parentNode.replaceChild(new_element, old_element);
-
-          // document.getElementById(`${x},${y}`).removeEventListener('click');
-          document.getElementById(`${x},${y}`).addEventListener('click', e => {
-            touched(e);
-          });
-        }
-      }
-    });
-  }
+  // if not yet chosen - calculate x and y coordinates of a piece to move
+  if (!x || !y)   {
+    // get the field ID depending if square or icon is clicked
+    const getSquareID = e.target.tagName === 'I' ? e.target.parentNode.id: e.target.id;
+    x = getSquareID[0];
+    y = getSquareID[2];
+    
+    // return if square is empty or if player tried to move wrong side
+    if (!board[x][y] || board[x][y].side !== currentColor) {
+      resetCoordinates();
+      return;
+    }
+    // get possible moves and mark squares on board, if no moves: return
+    possibleMoves = board[x][y].findLegalMoves();
+    if (possibleMoves.length === 0) {
+      resetCoordinates();
+      return
+    };
+    for (let el of possibleMoves) {
+      document.getElementById(el).className += ` possibleMove`;
+    }
+  } 
+  // the piece already chosen, player to decide where to move
+  else {
+    const chosenMove = e.target.tagName === 'I'? e.target.parentNode.id: e.target.id;
+    // proceed only if the move is legal, change the side
+    if (possibleMoves.includes(chosenMove)) {
+      board[x][y].move(chosenMove);
+      currentColor === 'white' ? currentColor = 'black': currentColor = 'white';
+    }; 
+    unmarkPossMoves(possibleMoves);
+    resetCoordinates();
+  }  
 };
 
 export default touched;
