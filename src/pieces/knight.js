440b2a811b8e4f20a3899fc7d23a00c7260cf835
Marcin <marcinzuchowski@wp.pl>
2019-08-19 23:45:10 +0200

    Knight uzupełniony o isPieceMoveCheckmate i findAttackMoves

--- src/pieces/knight.js
+++ src/pieces/knight.js
@@ -1,35 +1,41 @@
 import Piece from './piece';
 import board from '../board';
+import { isPieceMoveCheckmate } from './utils';
 
 class Knight extends Piece {
   constructor(x, y, side) {
     super(x, y, side);
     this.name = 'knight';
     this.display = `<i class="fas fa-chess-knight ${side}"></i>`; //fontawesome knight
-  }
+    this.knightJumpes = [[2,-1],[2,1],[1,-2],[1,2],[-2,-1],[-2,1],[-1,-2],[-1,2]];
+  };
 
   findLegalMoves() {
-    const oldX = this.x;
-    const oldY = this.y;
-    const thisSide = this.side;
     let newX, newY;
     const possibleMoves = [];
-    const jumpes = [[2, -1], [2, 1], [1, -2], [1, 2], [-2, -1], [-2, 1], [-1, -2], [-1, 2]];
-    function checkLegal(item) {
-      newX = oldX + item[0];
-      newY = oldY + item[1];
+    const checkLegal= (item) => {
+      newX = this.x + item[0];
+      newY = this.y + item[1];
       if (newX >= 0 && newX < 8 && newY >= 0 && newY < 8) {
-        !board[newX][newY] || board[newX][newY].side !== thisSide ? possibleMoves.push(`${newX},${newY}`) : '';
+        (!board[newX][newY] || board[newX][newY].side !== this.side ) && !isPieceMoveCheckmate(this, newX, newY)? possibleMoves.push(`${newX},${newY}`):'';
       }
     }
-    jumpes.forEach(checkLegal);
+    this.knightJumpes.forEach(checkLegal);
 
     return possibleMoves;
   }
 
   findAttackMoves() {
-    /* TODO */
-    return [];
+    let newX, newY;
+    const attackMoves = [];
+    const checkAttack= (item) => {
+      newX = this.x + item[0];
+      newY = this.y + item[1];
+      newX >= 0 && newX < 8 && newY >= 0 && newY < 8 ? attackMoves.push(`${newX},${newY}`):'';
+    }
+    this.knightJumpes.forEach(checkAttack);
+
+    return attackMoves;
   }
 }
 
